649694845e5aa8f271ac1c03e4a0a9ba72629157
MGonza20 <sara.paguaga@gmail.com>
2022-08-17 16:41:22 -0600

    Refactoring controller

--- backend/controllers/vetController.js
+++ backend/controllers/vetController.js
@@ -1,37 +1,39 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get vets info
 // @route   GET /api/vets
 // @access  Public
-const getVets = (req, res) => {
+const getVets = asyncHandler(async(req, res) => {
     res.status(200).json({message:'Get vets info'})
-}
+})
 
 // @desc    Set vet info
 // @route   POST /api/vets
 // @access  Private
-const setVetinfo = (req, res) => {
+const setVetinfo = asyncHandler (async(req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json({message:'Set vet info'})
-}
+})
 
 
 // @desc    Update vet info
 // @route   PUT /api/vets/:id
 // @access  Private
-const updateVetinfo = (req, res) => {
+const updateVetinfo = asyncHandler( async(req, res) => {
     res.status(200).json({message:`Update vet ${req.params.id}`})
-}
+})
 
 
 // @desc    Delete vet info
 // @route   DELETE /api/vets:id
 // @access  Private
-const deleteVetinfo = (req, res) => {
+const deleteVetinfo = asyncHandler( async(req, res) => {
     res.status(200).json({message:`Delete vet ${req.params.id}`})
-}
+})
 
 
 
