6a8c89daf5b178f63ecdc108e70424b9ba859194
alv18051 <alv18051@uvg.edu.gt>
2022-08-19 21:29:57 -0600

    cambios en el formulario para mantener cohesion con la bd

--- src/pages/RegisterVet.js
+++ src/pages/RegisterVet.js
@@ -3,30 +3,45 @@ import InputComponent from './components/InputComponent'
 import { Heading, Button } from '@chakra-ui/react'
 import '../styles/form.css'
 
-let user_type = 'vet'
-
 const RegisterVet = () => {
     const [nombre, setNombre] = useState('')
-    const [apellido, setApellido] = useState('')
-    const [correo, setCorreo] = useState('')
-    const [numCol, setNumCol] = useState('')
-    const [nombreVet, setNombreVet] = useState('')
+    const [ciudad, setCiudad] = useState('')
+    const [zona, setZona] = useState('')
     const [direccion, setDireccion] = useState('')
-    const [face, setFace] = useState('')
-    const [pagina, setPagina] = useState('')
+    const [correo, setCorreo] = useState('')
+    const [servicios, setServicios] = useState([])
+    const [latitud, setLatitud] = useState('')
+    const [longitud, setLongitud] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [emergencia, setEmergencia] = useState('')
+    const [tipo, setTipo] = useState('')
+    const [apertura, setApertura] = useState('')
+    const [cierre, setCierre] = useState('')
+    
 
-    const handleAddUser = (user_name, email, password, type_user) => {
-        fetch('http://127.0.0.1:8000/add_user', {
+    const handleAddVet = () => {
+        fetch('http://127.0.0.1:8000/api/vets', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                user_name: user_name,
-                correo: email,
-                password: password,
-                type_user: type_user,
+                nombre: nombre,
+                ciudad: ciudad,
+                zona: zona,
+                direccion: direccion,
+                correo: correo,
+                servicios: servicios,
+                latitud: latitud,
+                longitud: longitud,
+                telefono: telefono,
+                emergencia: emergencia,
+                tipo: tipo,
+                apertura: apertura,
+                cierre: cierre,
+                verified: false,
             }),
+
         })
             .then((response) => response.json())
             .then((result) => {
@@ -35,9 +50,10 @@ const RegisterVet = () => {
                 } else {
                     alert('Error con la solicitud')
                 }
-            })
-            .catch((error) => {
+            }).catch((error) => {
                 alert('Ocurrio un error inesperado: ' + error)
+            }).then(() => {
+                window.location.href = '/'
             })
     }
 
@@ -50,30 +66,52 @@ const RegisterVet = () => {
         setNombre(name)
     }
 
-    const getApellido = (apellido) => {
-        setApellido(apellido)
-    }
-    const getCorreo = (correo) => {
-        setCorreo(correo)
-    }
-    const getNumCol = (numCol) => {
-        setNumCol(numCol)
+    const getCiudad = (ciudad) => {
+        setCiudad(ciudad)
     }
 
-    const getNombreVet = (nombreVet) => {
-        setNombreVet(nombreVet)
+    const getZona = (zona) => {
+        setZona(zona)
     }
 
     const getDireccion = (address) => {
         setDireccion(address)
     }
 
-    const getFace = (face) => {
-        setFace(face)
+    const getCorreo = (correo) => {
+        setCorreo(correo)
     }
 
-    const getWeb = (url) => {
-        setPagina(url)
+    const getServicios = (servicios) => {
+        setServicios(servicios)
+    }
+
+    const getLatitud = (latitud) => {
+        setLatitud(latitud)
+    }
+
+    const getLongitud = (longitud) => {
+        setLongitud(longitud)
+    }
+
+    const getTelefono = (telefono) => {
+        setTelefono(telefono)
+    }
+
+    const getEmergencia = (emergencia) => { 
+        setEmergencia(emergencia)
+    }
+
+    const getTipo = (tipo) => {
+        setTipo(tipo)
+    }
+
+    const getApertura = (apertura) => {
+        setApertura(apertura)
+    }
+
+    const getCierre = (cierre) => {
+        setCierre(cierre)
     }
 
     return (
@@ -81,49 +119,86 @@ const RegisterVet = () => {
             <div className="outerContainer container">
                 <div className="infoContainer">
                     <div className="titleContainer">
-                        <Heading className="title">Formulario de registro</Heading>
+                        <Heading className="title">Formulario para registrar una veterinaria</Heading>
                     </div>
                     <form onSubmit={handleSubmit}>
                         <InputComponent
                             getter={getNombre}
                             title="Nombre"
-                            message="Ingresa tu nombre"
+                            message="Ingresa el nombre del veterinario"
                         />
                         <InputComponent
-                            getter={getApellido}
-                            title="Apellidos"
-                            message="Ingresa tus apellidos"
+                            getter={getCiudad}
+                            title="Ciudad"
+                            message="Ingresa la ciudad de tu veterinaria"
                         />
+
+                        <InputComponent
+                            getter={getZona}
+                            title="Zona"
+                            message="Ingresa la zona de tu veterinaria"
+                        />
+
+                        <InputComponent
+                            getter={getDireccion}
+                            title="Dirección"
+                            message="Ingresa la dirección de tu veterinaria"
+                        />
+
                         <InputComponent
                             getter={getCorreo}
                             title="Correo"
                             message="Ingresa tu correo"
                         />
+
                         <InputComponent
-                            getter={getNumCol}
-                            title="Numero de colegiado"
-                            message="Ingrese su numero de colegiado"
+                            getter={getServicios}
+                            title="Servicios"
+                            message="Ingresa los servicios que ofreces"
                         />
+
                         <InputComponent
-                            getter={getNombreVet}
-                            title="Nombre de la veterinaria"
-                            message="Ingresa el nombre de la veterinaria"
+                            getter={getLatitud}
+                            title="Latitud"
+                            message="Ingresa la latitud de tu veterinaria"
                         />
+
                         <InputComponent
-                            getter={getDireccion}
-                            title="Dirección de la veterinaria"
-                            message="Ingresa la dirección de la veterinaria"
+                            getter={getLongitud}
+                            title="Longitud"
+                            message="Ingresa la longitud de tu veterinaria"
                         />
+
                         <InputComponent
-                            getter={getFace}
-                            title="Pagina de Facebook"
-                            message="Ingresa la pagina de Facebook de la veterinaria"
+                            getter={getTelefono}
+                            title="Teléfono"
+                            message="Ingresa el teléfono de tu veterinaria"
                         />
+
                         <InputComponent
-                            getter={getWeb}
-                            title="Pagina web"
-                            message="Ingresa la pagina web de la veterinaria"
+                            getter={getEmergencia}
+                            title="Emergencia"
+                            message="Atiendes emergencias 24H?"
                         />
+
+                        <InputComponent
+                            getter={getTipo}
+                            title="Tipo"
+                            message="Ingresa el tipo de veterinaria (Tienda, Clinica, Hospital)"
+                        />
+
+                        <InputComponent
+                            getter={getApertura}
+                            title="Apertura"
+                            message="Ingresa la hora de apertura"
+                        />
+
+                        <InputComponent
+                            getter={getCierre}
+                            title="Cierre"
+                            message="Ingresa la hora de cierre"
+                        />
+
                         <Button
                             backgroundColor="#ea9a64"
                             _hover="rgb(174 213 142)"
@@ -139,6 +214,14 @@ const RegisterVet = () => {
                             Enviar formulario
                         </Button>
                     </form>
+
+                    <p className="questionCont">
+                        {' '}
+                        <a href="/">
+                            {' '}
+                            <b className="highlight">¿Regresar?</b>
+                        </a>
+                    </p>
                 </div>
 
                 <div className="innerContainer"></div>
