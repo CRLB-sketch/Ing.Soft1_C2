24a59d2b2d2d8182d311bef420dac1dff2a0a6b7
alv18051 <alv18051@uvg.edu.gt>
2022-08-22 17:00:33 -0600

    Corte 1 - ING 2

--- src/pages/Search.test.js
+++ src/pages/Search.test.js
@@ -1,9 +1,3 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-undef */
-/* eslint-disable react/jsx-filename-extension */
-/**
-* @jest-enviroment jsdom
-*/
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Search from './Search'
