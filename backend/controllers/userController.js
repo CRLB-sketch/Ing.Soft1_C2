36648ee1b54246f6b96aefc45c4e03fe33ba8f8b
MGonza20 <sara.paguaga@gmail.com>
2022-09-25 16:25:54 -0600

    auth to get info depending on token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
@@ -37,6 +37,7 @@ const registerUser = asyncHandler(async(req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id)
         })
     } 
     else {
@@ -57,7 +58,8 @@ const loginUser = asyncHandler( async(req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -67,10 +69,18 @@ const loginUser = asyncHandler( async(req, res) => {
 
 // @desc   Get user data
 // @route  GET /api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler( async(req, res) => {
     res.json({message: 'User Info'})
 })
+
+// Generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
 
 module.exports = {
     registerUser,
