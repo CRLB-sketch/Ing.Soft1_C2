e257966fddfab29d44062c2a2bffad45fee7722f
MGonza20 <sara.paguaga@gmail.com>
2022-09-26 01:18:00 -0600

    making some updates in register and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,58 +1,41 @@
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react'
-import InputComponent from './components/InputComponent'
+import { Input, FormLabel } from '@chakra-ui/react'
 import { Heading, Button } from '@chakra-ui/react'
 import '../styles/register.css'
 import HeaderComponent from './components/HeaderComponent'
 
 const Login = () => {
-    const [correo, setCorreo] = useState('')
-    const [contra1, setContra1] = useState('')
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
 
-    const getCorreo = (correo) => {
-        setCorreo(correo)
-    }
-    const getContra = (contra) => {
-        setContra1(contra)
-    }
+    const {email, password} = formData
 
-    const handleVerify = (email, password) => {
-        fetch('http://127.0.0.1:8000/verify', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
-        })
-            .then((response) => response.json())
-            .then((result) => {
-                if (result.success) {
-                    console.log(result.exist)
-                    console.log(result.data)
-                    if (result.exist === 0) {
-                        alert('USuario o contraseña no valido')
-                        console.log('yes')
-                    } else {
-                        alert('Bienvenido')
-                        console.log('no')
-                    }
-                } else {
-                    alert('Error con la solicitud')
-                }
-            })
-            .catch((error) => {
-                alert('Ocurrio un error inesperado: ' + error)
-            })
+    const colors = {
+        fondo: 'rgb(223 225 225)',
+        verde: 'rgb(174 213 142)',
+        white: 'rgb(181 142 213)',
+        naranja: 'rgb(37, 150, 190)',
+        verde2: '#97db75',
+        verde3: '#ace291',
+        verde4: '#b6e69e',
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        handleVerify(correo, contra1)
+    const handleChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState, 
+            [e.target.name]: e.target.value,
+        }))
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+    
     return (
         <div>        
             <HeaderComponent className="header" />
@@ -62,17 +45,32 @@ const Login = () => {
                         <div className="titleContainer">
                             <Heading className="title">Iniciar Sesión</Heading>
                         </div>
-                        <form onSubmit={handleSubmit}>
-                            <InputComponent
-                                getter={getCorreo}
-                                title="Correo"
-                                message="Ingresa tu correo"
-                            />
-                            <InputComponent
-                                getter={getContra}
-                                title="Contraseña"
-                                message="Ingresa tu contraseña"
-                            />
+                        <form onSubmit={onSubmit}>
+
+                            <div className="outerContainer2">
+                                <FormLabel>{'Correo'}</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={email}
+                                    name='email'
+                                    onChange={handleChange}
+                                    focusBorderColor={colors.verde}
+                                    placeholder={'Ingrese su correo'}
+                                />
+                            </div>
+
+                            <div className="outerContainer2">
+                                <FormLabel>{'Contraseña'}</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={password}
+                                    name='password'
+                                    onChange={handleChange}
+                                    focusBorderColor={colors.verde}
+                                    placeholder={'Ingrese su contraseña'}
+                                />
+                            </div>
+
                             <Button
                                 backgroundColor="#ea9a64"
                                 _hover="rgb(174 213 142)"
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,138 +1,124 @@
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react'
-import InputComponent from './components/InputComponent'
+import { Input, FormLabel } from '@chakra-ui/react'
 import { Heading, Button } from '@chakra-ui/react'
 import '../styles/register.css'
-
-let user_type = 'vet'
+import HeaderComponent from './components/HeaderComponent'
 
 const Register = () => {
-    // const [nombre, setNombre] = useState('')
-    // const [apellido, setApellido] = useState('')
-    const [user, setUser] = useState('')
-    const [correo, setCorreo] = useState('')
-    const [contra1, setContra1] = useState('')
-    const [contra2, setContra2] = useState('')
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
 
-    const handleAddUser = (user_name, email, password, type_user) => {
-        fetch('http://127.0.0.1:8000/add_user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                user_name: user_name,
-                correo: email,
-                password: password,
-                type_user: type_user,
-            }),
-        })
-            .then((response) => response.json())
-            .then((result) => {
-                if (result.success) {
-                    alert('Se agrego el user')
-                } else {
-                    alert('Error con la solicitud')
-                }
-            })
-            .catch((error) => {
-                alert('Ocurrio un error inesperado: ' + error)
-            })
+    const {name, email, password, password2} = formData
+
+    const colors = {
+        fondo: 'rgb(223 225 225)',
+        verde: 'rgb(174 213 142)',
+        white: 'rgb(181 142 213)',
+        naranja: 'rgb(37, 150, 190)',
+        verde2: '#97db75',
+        verde3: '#ace291',
+        verde4: '#b6e69e',
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        if (contra1 === contra2) {
-            handleAddUser(user, correo, contra1, user_type)
-        } else {
-            alert('no coinciden las contraseñas, vuelva a intentarlo')
-        }
+    const handleChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState, 
+            [e.target.name]: e.target.value,
+        }))
     }
 
-    // const getNombre = (name) => {
-    //     setNombre(name)
-    // }
-
-    // const getApellido = (apellido) => {
-    //     setApellido(apellido)
-    // }
-    const getUser = (user) => {
-        setUser(user)
+    const onSubmit = (e) => {
+        e.preventDefault()
     }
-    const getCorreo = (correo) => {
-        setCorreo(correo)
-    }
-    const getContra = (contra) => {
-        setContra1(contra)
-    }
-    const getContras = (contras) => {
-        setContra2(contras)
-    }
-
+    
     return (
-        <div className="provisionalBackgorund">
-            <div className="outerContainer container">
-                <div className="infoContainer">
-                    <div className="titleContainer">
-                        <Heading className="title">Registro</Heading>
+        <div>        
+            <HeaderComponent className="header" />
+            <div className="provisionalBackgorund">
+                <div className="outerContainer container">
+                    <div className="infoContainer">
+                        <div className="titleContainer">
+                            <Heading className="title">Iniciar Sesión</Heading>
+                        </div>
+                        <form onSubmit={onSubmit}>
+
+                            <div className="outerContainer2">
+                                <FormLabel>{'Nombre'}</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={name}
+                                    name='name'
+                                    onChange={handleChange}
+                                    focusBorderColor={colors.verde}
+                                    placeholder={'Ingrese su nombre'}
+                                />
+                            </div>
+
+                            <div className="outerContainer2">
+                                <FormLabel>{'Correo'}</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={email}
+                                    name='email'
+                                    onChange={handleChange}
+                                    focusBorderColor={colors.verde}
+                                    placeholder={'Ingrese su correo'}
+                                />
+                            </div>
+
+                            <div className="outerContainer2">
+                                <FormLabel>{'Contraseña'}</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={password}
+                                    name='password'
+                                    onChange={handleChange}
+                                    focusBorderColor={colors.verde}
+                                    placeholder={'Ingrese su contraseña'}
+                                />
+                            </div>
+
+                            <div className="outerContainer2">
+                                <FormLabel>{'Confirmar contraseña'}</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={password2}
+                                    name='password2'
+                                    onChange={handleChange}
+                                    focusBorderColor={colors.verde}
+                                    placeholder={'Ingrese nuevamente su contraseña'}
+                                />
+                            </div>
+
+                            <Button
+                                backgroundColor="#ea9a64"
+                                _hover="rgb(174 213 142)"
+                                _active={{
+                                    bg: 'rgb(174 213 142)',
+                                    borderColor: 'rgb(174, 213, 142)',
+                                }}
+                                color="#fff"
+                                type="submit"
+                                width="100%"
+                                marginTop="10px"
+                            >
+                                Aceptar
+                            </Button>
+                        </form>
+                        <p className="questionCont">
+                            ¿No tienes cuenta?{' '}
+                            <a href="./Login>">
+                                <b className="highlight">¡Registrate!</b>
+                            </a>
+                        </p>
                     </div>
-                    <form onSubmit={handleSubmit}>
-                        <InputComponent
-                            // getter={getNombre}
-                            title="Nombre"
-                            message="Ingresa tu nombre"
-                        />
-                        <InputComponent
-                            // getter={getApellido}
-                            title="Apellidos"
-                            message="Ingresa tus apellidos"
-                        />
-                        <InputComponent
-                            getter={getUser}
-                            title="Nombre de usuario"
-                            message="Ingresa tu nombre de usuario"
-                        />
-                        <InputComponent
-                            getter={getCorreo}
-                            title="Correo"
-                            message="Ingresa tu correo"
-                        />
-                        <InputComponent
-                            getter={getContra}
-                            title="Contraseña"
-                            message="Ingresa tu contraseña"
-                        />
-                        <InputComponent
-                            getter={getContras}
-                            title="Cofirmar contraseña"
-                            message="Confirma tu contraseña"
-                        />
-                        <Button
-                            backgroundColor="#ea9a64"
-                            _hover="rgb(174 213 142)"
-                            _active={{
-                                bg: 'rgb(174 213 142)',
-                                borderColor: 'rgb(174, 213, 142)',
-                            }}
-                            color="#fff"
-                            type="submit"
-                            width="100%"
-                            marginTop="10px"
-                        >
-                            Aceptar
-                        </Button>
-                    </form>
-
-                    <p className="questionCont">
-                        ¿Ya tienes cuenta?{' '}
-                        <a href="./Login">
-                            {' '}
-                            <b className="highlight">Iniciar sesión</b>
-                        </a>
-                    </p>
+                    <div className="innerContainer"></div>
                 </div>
-
-                <div className="innerContainer"></div>
             </div>
         </div>
     )
